b95f75502c801de016d00f92b6229b8745aaa854
AMIT SINGH RAWAT <53403458+Amitrawat12@users.noreply.github.com>
2025-06-13 00:04:12 +0530

    Time: 1 ms (91.87%), Space: 61.4 MB (6.86%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.js
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.js
@@ -6,18 +6,14 @@ var findMaxConsecutiveOnes = function(nums) {
     let currentcount = 0;
     let maxcount = 0;
     for(let i = 0; i<nums.length; i++){
-        if(nums[i]!=0 ){
+        if(nums[i]==1){
             currentcount++;
         }
         else{
-            if(maxcount<currentcount){
-                maxcount=currentcount
-            }
+            maxcount = Math.max(maxcount,currentcount)
             currentcount = 0;
         }
     }
-    if(maxcount<currentcount){
-        maxcount=currentcount
-    }
-    return maxcount
+    
+    return Math.max(maxcount,currentcount)
 };
