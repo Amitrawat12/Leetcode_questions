0da0a2fd01d66eb2dd79470f27d50abf3bdb45fc
AMIT SINGH RAWAT <53403458+Amitrawat12@users.noreply.github.com>
2025-06-14 22:40:09 +0530

    Time: 49 ms (48.28%), Space: 55.4 MB (5.15%) - LeetHub

---
+++ 1013-fibonacci-number/1013-fibonacci-number.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function(n) {
+    let a = 0;
+    let b = 1;
+    let c =0;
+    if(n == 0) return a;
+    if(n == 1) return b;
+    for(let i = 2; i<=n;i++){
+        c=a+b;
+        a=b;
+        b=c;
+    }
+    return c;
+};
